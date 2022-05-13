752e6943adbb2b2563b909456f2ae85022e45275
IssamMerzagua <merzagua.issam@gmail.com>
2022-05-13 20:42:30 +0200

    Sécurité

---
+++ Banque/src/main/java/fr/insa/issam/securite/BasicConfiguration.java
@@ -0,0 +1,39 @@
+//package fr.insa.issam.securite;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    	PasswordEncoder encoder = 
+//          PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    	auth
+//          .inMemoryAuthentication()
+//          .withUser("issam")
+//          .password(encoder.encode("issam"))
+//          .roles("ADMIN");
+////          .and()
+////          .withUser("merzagua")
+////          .password(encoder.encode("merzagua"))
+////          .roles("USER", "ADMIN");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//          .authorizeRequests()
+//          .anyRequest()
+//          .authenticated()
+//          .and()
+//          .httpBasic();
+//    }
+//}
