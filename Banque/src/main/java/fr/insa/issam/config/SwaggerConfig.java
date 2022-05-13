3980c72e2e3a0be32518876fc9386ed604e72992
IssamMerzagua <merzagua.issam@gmail.com>
2022-05-14 00:12:11 +0200

    Un Swagger pou la génération automatique de la documentation à partir du code. ( API pour gérer les comptes bancaires des clients )

---
+++ Banque/src/main/java/fr/insa/issam/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package fr.insa.issam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("fr.insa.issam.controleur"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    // Describe your apis
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Banque API")
+                .description("API to manage clients accounts bank")
+                .version("0.0.1-SNAPSHOT")
+                .build();
+    }
+}
