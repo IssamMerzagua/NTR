252c85a02de9708955a1f4faa07de655fd79cc08
IssamMerzagua <merzagua.issam@gmail.com>
2022-05-15 21:19:05 +0200

    Le Service Gateway

---
+++ ms-gateway/src/main/java/fr/gateway/SpringCloudConfig.java
@@ -0,0 +1,30 @@
+package fr.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Autowired
+    private JwtCustomFilter customJwtFilter;
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/etudiants/**")
+                        .filters(f ->
+                                f.addRequestHeader("header-1", "value1")
+                                 .addResponseHeader("header-response-1", "value-response-1")
+                                 )
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/formations/**")
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/**")
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+}
